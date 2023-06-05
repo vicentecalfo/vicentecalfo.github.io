aa889e5033c9dfb2624b9af3319ce87e0f15f24f
Vicente Calfo <vicentecalfo@gmail.com>
2023-06-05 10:10:57 -0300

    Update CSP Giscus

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src 'self' https://giscus.app
+  frame-src https://giscus.app
+  frame-ancestors  'self' https://giscus.app
 `
 
 const securityHeaders = [
