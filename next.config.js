0c31b15a58f3c23552c3e7289ef32d1ef8cb538c
Vicente Calfo <vicentecalfo@gmail.com>
2023-06-05 11:13:44 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app;
-  frame-ancestors 'self';
+  frame-src https://giscus.app;
+  frame-ancestors 'self' https://giscus.app;
 `
 
 const securityHeaders = [
